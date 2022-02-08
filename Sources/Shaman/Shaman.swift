5a169cf1c1fd7e0a4607d4eb653de4b0fe2cf838
Alexander Cyon <alex.cyon@gmail.com>
2022-02-08 09:15:41 +0100

    Introduce tags and simplify solution

--- Sources/Shaman/Shaman.swift
+++ Sources/Shaman/Shaman.swift
@@ -30,17 +30,127 @@ public enum Error: Swift.Error, Equatable {
     case incorrectSizeOfFixedMidstate(got: Int, butExpected: Int)
 }
 
+func byteArray<T>(from value: T) -> [UInt8] where T: FixedWidthInteger {
+    withUnsafeBytes(of: value.bigEndian, Array.init)
+}
+
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
+
+private func inspectInnerState(of hasher: secp256k1_sha256) -> Data {
+    var state = hasher.s
+    return Data(withUnsafeBytes(of: &state) { statePointer in
+        [UInt8](statePointer).chunked(into: 4)
+            .map ({
+                print("🔮 \($0.hexString)")
+                return $0.reversed()
+            })
+            .reduce([UInt8](), +)
+    })
+    
+}
+
+func inspectStateOf(hasher: SHA256) -> Data {
+    inspectInnerState(of: hasher.wrapper.hasher)
+}
+func inspectStateOf(tag: SHA256.Tag) -> Data {
+    inspectInnerState(of: tag.cachedState)
+}
+
+
+internal func == (lhs: secp256k1_sha256, rhs: secp256k1_sha256) -> Bool {
+    guard lhs.bytes == rhs.bytes else { return false }
+
+    var lhsBuf = lhs.buf
+    var rhsBuf = rhs.buf
+    guard (withUnsafeBytes(of: &lhsBuf) { lhsBufPointer in
+        withUnsafeBytes(of: &rhsBuf) { rhsBufPointer in
+            guard lhsBufPointer.count == rhsBufPointer.count else {
+                return false
+            }
+            for index in 0..<lhsBufPointer.count {
+                if lhsBufPointer[index] != rhsBufPointer[index] {
+                    return false
+                }
+            }
+            return true
+        }
+    }) else {
+        return false
+    }
+
+    var lhsState = lhs.s
+    var rhsState = rhs.s
+    guard (withUnsafeBytes(of: &lhsState) { lhsStatePointer in
+        withUnsafeBytes(of: &rhsState) { rhsStatePointer in
+            guard lhsStatePointer.count == rhsStatePointer.count else {
+                return false
+            }
+            for index in 0..<lhsStatePointer.count {
+                if lhsStatePointer[index] != rhsStatePointer[index] {
+                    return false
+                }
+            }
+            return true
+        }
+    }) else {
+        return false
+    }
+
+    return true
+}
+
+
+public extension SHA256 {
+    struct Tag: Equatable, CustomStringConvertible {
+        internal var cachedState: secp256k1_sha256
+        public let name: String
+        
+        internal init(cachedState: secp256k1_sha256, name: String) {
+            self.cachedState = cachedState
+            self.name = name
+        }
+        
+        fileprivate init(stateData: Data, description: String? = nil) throws {
+            guard stateData.count == 32 else {
+                throw Error.incorrectSizeOfFixedMidstate(got: stateData.count, butExpected: 32)
+            }
+            var hasher = secp256k1_sha256()
+            stateData.withUnsafeBytes { (dataPointer: UnsafeRawBufferPointer) -> Void in
+                secp256k1_sha256_init_with_state(&hasher, dataPointer.baseAddress, stateData.count)
+            }
+            self.name = description ?? "fixed midstate"
+            self.cachedState = hasher
+        }
+        
+        public static func == (lhs: Self, rhs: Self) -> Bool {
+            return lhs.cachedState == rhs.cachedState
+        }
+        
+        public var description: String {
+            "\(name) - state: \(inspectInnerState(of: cachedState))"
+        }
+    }
+}
+
 private final class Wrapper {
 //    typealias StateKey = String
 //    typealias State = ShamanDigest.State
 //    private var cachedStates: [StateKey: State] = [:]
-    private var hasher: secp256k1_sha256
+    fileprivate var hasher: secp256k1_sha256
 
     /// Initializes the hash function instance.
     public init() {
         self.hasher = secp256k1_sha256()
         initHasher()
     }
+    
+//    public init(fixedMidState: Data)
     
     @inlinable
     func initHasher() {
@@ -55,20 +165,15 @@ private final class Wrapper {
     @inlinable
     func update(
         bufferPointer inputPointer: UnsafeRawBufferPointer,
-        cacheStateIn pointerToCache: UnsafeMutableRawBufferPointer
-    ) throws {
-        guard pointerToCache.count == SHA256.Digest.byteCount else {
-            throw Error.incorrectSizeOfFixedMidstate(got: pointerToCache.count, butExpected: SHA256.Digest.byteCount)
-        }
-        secp256k1_sha256_write_cache_state(&hasher, inputPointer.baseAddress, inputPointer.count, pointerToCache.baseAddress)
+        tag: String
+    ) -> SHA256.Tag {
+        update(bufferPointer: inputPointer)
+        return .init(cachedState: hasher, name: tag)
     }
     
     @inlinable
-    func fixState(to bufferPointer: UnsafeRawBufferPointer) throws {
-        guard bufferPointer.count == SHA256.Digest.byteCount else {
-            throw Error.incorrectSizeOfFixedMidstate(got: bufferPointer.count, butExpected: SHA256.Digest.byteCount)
-        }
-        secp256k1_sha256_init_with_state(&hasher, bufferPointer.baseAddress, bufferPointer.count)
+    func restore(tag: SHA256.Tag) {
+        hasher = tag.cachedState
     }
     
     /// Returns the digest from the data input in the hash function instance.
@@ -150,7 +255,7 @@ public struct ShamanDigest: Crypto.Digest, DigestPrivate {
 
 public struct SHA256: Crypto.HashFunction {
     
-    private let wrapper: Wrapper
+    fileprivate let wrapper: Wrapper
 
     public init() {
         self.wrapper = Wrapper()
@@ -176,24 +281,32 @@ public extension SHA256 {
         wrapper.finalize()
     }
     
-    mutating func fixState(to bufferPointer: UnsafeRawBufferPointer) throws {
-        try wrapper.fixState(to: bufferPointer)
+    mutating func restore(tag: Tag) {
+        wrapper.restore(tag: tag)
+    }
+    
+    mutating func restore(state: Data, description: String? = nil) throws {
+        let tag = try Tag(stateData: state, description: description)
+        restore(tag: tag)
+    }
+    
+    mutating func update<D: DataProtocol>(
+        data bytes: D,
+        tag: String
+    ) -> Tag {
+        Data(bytes).withUnsafeBytes { dataPointer in
+            update(bufferPointer: dataPointer, tag: tag)
+        }
     }
     
     mutating func update(
         bufferPointer inputPointer: UnsafeRawBufferPointer,
-        cacheStateIn pointerToCache: UnsafeMutableRawBufferPointer
-    ) throws {
-        try wrapper.update(bufferPointer: inputPointer, cacheStateIn: pointerToCache)
+        tag: String
+    ) -> Tag {
+        wrapper.update(bufferPointer: inputPointer, tag: tag)
     }
     
-    @inlinable
-    mutating func fixState<D: DataProtocol>(to fixedMidStateDataProtocol: D) throws {
-        let fixedMidStateData = Data(fixedMidStateDataProtocol)
-        try fixedMidStateData.withUnsafeBytes { source in
-            try self.fixState(to: source)
-        }
-    }
+   
 }
 
 internal func safeCompare<LHS: ContiguousBytes, RHS: ContiguousBytes>(_ lhs: LHS, _ rhs: RHS) -> Bool {
--- Tests/ShamanTests/ShamanTests.swift
+++ Tests/ShamanTests/ShamanTests.swift
@@ -27,54 +27,75 @@ final class ShamanTests: XCTestCase {
     
     func test_fixState_to() throws {
         var hasher = SHA256()
-        try hasher.fixState(to: Data(hex: "46615b35f4bfbff79f8dc67183627ab3602171805735866121a29e5468b07b4c"))
+        try hasher.restore(state: Data(hex: "46615b35f4bfbff79f8dc67183627ab3602171805735866121a29e5468b07b4c"))
         XCTAssertEqual(hashBIP340Tag(), Data(hasher.finalize()))
     }
     
-    func test_update_cacheTo() throws {
+    func test_update_cacheTo() {
         var hasher = SHA256()
-        var cachedMidstate = Data(repeating: 0x00, count: 32)
-        let once = Data(SHA256.hash(data: bip340Tag.data(using: .utf8)!))
-        let data = Data(once + once)
         
-        try data.withUnsafeBytes { dataPointer in
-            try cachedMidstate.withUnsafeMutableBytes { targetPointer in
-                try hasher.update(
-                    bufferPointer: dataPointer,
-                    cacheStateIn: targetPointer
-                )
-            }
-        }
-    
-        XCTAssertEqual(cachedMidstate.hexString, "46615b35f4bfbff79f8dc67183627ab3602171805735866121a29e5468b07b4c")
-        try hasher.fixState(to: cachedMidstate)
+        let half = Data(SHA256.hash(data: "BIP0340/nonce".data(using: .utf8)!))
+        
+        let tag = hasher.update(
+            data: Data(half + half),
+            tag: "some description"
+        )
+        
+        XCTAssertEqual(inspectStateOf(tag: tag).hexString, inspectStateOf(hasher: hasher).hexString)
+        let expectedMidstate = "46615b35f4bfbff79f8dc67183627ab3602171805735866121a29e5468b07b4c"
+        XCTAssertEqual(inspectStateOf(tag: tag).hexString, expectedMidstate)
+        
+        let nonSenseInput = "non-sense data that must be 64 chars or longer, the purpose of which is to change the internal state of hasher"
+        XCTAssertGreaterThanOrEqual(nonSenseInput.count, 64) // must be GEQ than 64 because the hasher has an internal buffer with size 64.
+        hasher.update(data: nonSenseInput.data(using: .utf8)!)
+        XCTAssertNotEqual(inspectStateOf(hasher: hasher).hexString, expectedMidstate)
+        
+        // Restore
+        hasher.restore(tag: tag)
+        XCTAssertEqual(inspectStateOf(tag: tag).hexString, expectedMidstate)
+        
         XCTAssertEqual(hashBIP340Tag().hexString, Data(hasher.finalize()).hexString)
     }
     
-    func test_assert_that_caching_state_does_not_break_things() throws {
+    func test_assert_that_caching_state_does_not_break_things() {
         // https://www.di-mgt.com.au/sha_testvectors.html
         let expected = "cdc76e5c9914fb9281a1c7e284d73e67f1809a48a497200e046d39ccc7112cd0"
        
         var hasher = SHA256()
-        var midstate = Data(repeating: 0x00, count: 32)
-        var lastMidstate = Data(repeating: 0x00, count: 32)
+        var lastTag: SHA256.Tag!
         
         let input = String(repeating: "a", count: 1000).data(using: .utf8)!
         for _ in 0..<1000 {
-            try input.withUnsafeBytes { dataPointer in
-                try midstate.withUnsafeMutableBytes { targetPointer in
-                    try hasher.update(
-                        bufferPointer: dataPointer,
-                        cacheStateIn: targetPointer
-                    )
-                }
-            }
-            XCTAssertNotEqual(midstate, lastMidstate)
-            lastMidstate = midstate
+            let tag = hasher.update(data: input, tag: "irrelevant tag")
+            XCTAssertNotEqual(tag, lastTag)
+            lastTag = tag
         }
         
         XCTAssertEqual(Data(hasher.finalize()).hexString, expected)
         
+    }
+    
+    func test_assert_cache_state_various_padding() throws {
+        // https://www.di-mgt.com.au/sha_testvectors.html
+        let expected = "cdc76e5c9914fb9281a1c7e284d73e67f1809a48a497200e046d39ccc7112cd0"
+        let length = 1_000_000
+        
+        for bytesLeft in 1..<100 {
+            var referenceHasher = SHA256()
+            var hasherUnderTest = SHA256()
+            
+            let initialInput = String(repeating: "a", count: length - bytesLeft).data(using: .utf8)!
+            let midStateTag = referenceHasher.update(data: initialInput, tag: "initial input")
+            let remainingInput = String(repeating: "a", count: bytesLeft).data(using: .utf8)!
+            
+            XCTAssertEqual(initialInput.count + remainingInput.count, length)
+            
+            hasherUnderTest.restore(tag: midStateTag)
+            hasherUnderTest.update(data: remainingInput)
+            XCTAssertEqual(Data(hasherUnderTest.finalize()).hexString, expected)
+            referenceHasher.update(data: remainingInput)
+            XCTAssertEqual(Data(referenceHasher.finalize()).hexString, expected)
+        }
     }
 }
 
